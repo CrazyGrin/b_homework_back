3a45974d0539318ed3dc0544eee0178997a56d25
CrazyGrin <894355833@qq.com>
2017-04-20 21:52:28 +0800

    add

--- StuInfo/StuModel.php
+++ StuInfo/StuModel.php
@@ -16,27 +16,27 @@ class StuModel extends Model
 
 	public function showAllStu($classNum){
 
-		// $curl = curl_init();
-		// $header = array("content-type: application/x-www-form-urlencoded; charset=UTF-8");
-		// curl_setopt($curl, CURLOPT_URL, "http://jwzx.cqupt.edu.cn/jwzxtmp/showBjStu.php?bj={$classNum}");
-		// curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
-		// curl_setopt($curl, CURLOPT_HEADER, 0);
-		// curl_setopt($curl, CURLOPT_HTTPHEADER,$header);
-		// $content = curl_exec($curl);
-		// curl_close($curl);
+		$curl = curl_init();
+		$header = array("content-type: application/x-www-form-urlencoded; charset=UTF-8");
+		curl_setopt($curl, CURLOPT_URL, "http://jwzx.cqupt.edu.cn/jwzxtmp/showBjStu.php?bj={$classNum}");
+		curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+		curl_setopt($curl, CURLOPT_HEADER, 0);
+		curl_setopt($curl, CURLOPT_HTTPHEADER,$header);
+		$content = curl_exec($curl);
+		curl_close($curl);
 
-		// $reSet = "/(?<=<td>).*?(?=<\/td>)/";
-		// $reSet_content = [];
+		$reSet = "/(?<=<td>).*?(?=<\/td>)/";
+		$reSet_content = [];
 
-		// preg_match_all($reSet, $content, $reSet_content);
+		preg_match_all($reSet, $content, $reSet_content);
 
-		// $finalData = array_chunk($reSet_content[0], 10);
+		$finalData = array_chunk($reSet_content[0], 10);
 
-		// for ($i=1; $i < count($reSet_content[0])/10; $i++) {
-		// 	DB::insert('insert into stuinfo (stu_id, stu_num, stu_name, stu_sex, stu_classnum, stu_majornum, stu_majorname, stu_academy, stu_grade, stu_status) values (?,?,?,?,?,?,?,?,?,?)', $finalData[$i]);
-		// 	var_dump($finalData[$i]);
-		// 	echo "Insert Success";
-		// }
+		for ($i=1; $i < count($reSet_content[0])/10; $i++) {
+			DB::insert('insert into stuinfo (stu_id, stu_num, stu_name, stu_sex, stu_classnum, stu_majornum, stu_majorname, stu_academy, stu_grade, stu_status) values (?,?,?,?,?,?,?,?,?,?)', $finalData[$i]);
+			var_dump($finalData[$i]);
+			echo "Insert Success";
+		}
 
 	}
 	public function showPic($stuNum){
